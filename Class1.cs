329671d6fb277802c3fa77ea6ca7ecc53d3aa1a3
Vicente Pastor <persiafighter@gmail.com>
2017-10-24 12:04:08 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using Rocket.Core.Plugins;
+
+namespace HideConfig
+{
+    public class Class1 : RocketPlugin
+    {
+        protected override void Load() => Rocket.Core.R.Plugins.OnPluginsLoaded += () =>
+        {
+            Steamworks.SteamGameServer.SetKeyValue("Browser_Config_Count", "0");
+        };
+    }
+}
